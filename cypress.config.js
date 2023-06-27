fd1bedfbaa8fe85f00245c5c2d27050bd3382014
Uvira007 <84002707+Uvira007@users.noreply.github.com>
2023-06-27 14:38:56 -0600

    updater the base Url

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   'projectId': '4b7344',
   e2e: {
-    "baseUrl": "http://localhost:8080",
+    "baseUrl": "https://example.cypress.io",
     "env":{
       "navbarText": "cypress.io"
     }
